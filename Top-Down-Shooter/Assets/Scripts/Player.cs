3ba6cef2ce81e84d4be0755d6be0a9f077bc6d98
Youcef Benslimane <youyou.dbz@gmail.com>
2019-09-09 17:54:06 +0200

    Added the player movement and projectiles

---
+++ Top-Down-Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	
+	public float speed;
+	
+	private Rigidbody2D rb;
+	
+	private Animator playerAnim;
+	
+	private Vector2 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+		playerAnim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+		float moveY = Input.GetAxisRaw("Vertical");
+		
+		move = new Vector2(moveX, moveY) * speed * Time.deltaTime;
+		
+		if(move != Vector2.zero){
+			playerAnim.SetBool("isRunning", true);
+            
+		}
+		else{
+			playerAnim.SetBool("isRunning", false);
+		}
+		
+    }
+	void FixedUpdate(){
+		rb.MovePosition(rb.position + move);
+	}
+}
---
+++ Top-Down-Shooter/Assets/Scripts/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject projectile;
+
+    public Transform shootingPosition;
+
+    public float cooldown;
+    private float cd;
+
+
+    void Update()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        transform.rotation = rotation;
+
+
+
+        if (Input.GetMouseButton(0))
+        {
+            if(Time.time >= cd)
+            {
+                Instantiate(projectile, shootingPosition.position, transform.rotation);
+                cd = Time.time + cooldown; 
+            }
+
+        }
+    }
+}
